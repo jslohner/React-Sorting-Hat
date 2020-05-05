b2edee6f35d7787a369f95ae201e74adf6338c0b
Justin Lohner <lohner4@gmail.com>
2020-05-05 09:12:40 -0600

    Got all of the questions mapped out on the questions page

--- react-sorting-hat/src/components/Question.js
+++ react-sorting-hat/src/components/Question.js
@@ -4,7 +4,14 @@ class Question extends React.Component {
 	render() {
 		return (
 			<div>
-
+				<h2>{this.props.questionData.question}</h2>
+				{
+					Object.keys(this.props.questionData.answers).map(answer => {
+						return (
+							<label key={`${answer} - ${this.props.questionData.id}`}>{answer} - <input type='checkbox' name='answer'/></label>
+						);
+					})
+				}
 			</div>
 		);
 	}
--- react-sorting-hat/src/components/QuestionsPage.js
+++ react-sorting-hat/src/components/QuestionsPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Question from './Question.js';
+
 class QuestionsPage extends React.Component {
 	render() {
 		return (
 			<div>
-
+				{
+					this.props.sortingQuestions.map(questionData => {
+						return <Question key={questionData.id} questionData={questionData}/>
+					})
+				}
 			</div>
 		);
 	}
